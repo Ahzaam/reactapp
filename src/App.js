c7f796aaaaf47dfdebc291b1d45c7204c6c217a1
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-12 18:49:28 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// Person function
+
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name :{props.name}</h1>
+      <h2>Last Name :{props.name2}</h2>
+    </>
+  )
+}
+
+
+// App
 function App() {
+  const name = 'Ahzam'
+  const name2 = 'A380'
+  const isTrue  = false
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 cla>Hello {name} {2 + 2}!</h1>
+      {isTrue ?(
+        <>
+          test
+        </>
+        ): (
+          <>
+            <h1>hi {name}!</h1>
+            <h2>There in no name</h2>
+            <Person name={name} name2={name2}/>
+
+            <Person name={name} name2={name}/>
+            
+          </>
+          
+        )} 
     </div>
   );
 }
