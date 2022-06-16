ef4fe79dc7b6437a308d75f4ce4c91f4b25a1eff
Ahzaam <99543407+Ahzaam@users.noreply.github.com>
2022-06-16 06:45:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import SearchIcon from "./search.svg";
 
 const API_URL = 'https://www.omdbapi.com?apikey=547d4cbc';
 
-const movie1 = {Title: 'Maverick', 
-    Year: '1994', 
-    imdbID: 'tt0110478', 
-    Type: 'movie', 
-    Poster: "https://m.media-amazon.com/images/M/MV5BY2I1OTY2NmUtMGVlZi00NjNmLThkNTgtMjExMzRhOTM2MDJiXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
-    }
 
 
 const App = () => {
